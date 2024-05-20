0a0fa6a17e075b1d9fb0367cfbd0cf8d0f56940f
Sam Steimer <samsteimer@Sams-Laptop.local>
2024-05-20 12:07:26 -0400

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for a city or state" class="py-2 px-1 w-full bg- bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+      <ul  
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults">
+        <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+        <p v-if="!serverError && mapboxSearchResults.length === 0">No results match your query, try a different term.</p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id"
+          class="py-2 cursor-pointer"
+          >
+          {{ searchResult.properties.full_address }}
+        </li>
+      </template>
+      </ul>
+    </div>
+    
+  </main>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+const mapboxAPIKey = "pk.eyJ1Ijoic3RlaW1lciIsImEiOiJjbHdmM2N4ankwamoyMnFtdHM3bTR3aHFqIn0.sa3_ERn4jpEPamb1YI4NlQ";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(`https://api.mapbox.com/search/geocode/v6/forward?q=${searchQuery.value}&access_token=${mapboxAPIKey}&types=place`);
+        mapboxSearchResults.value = result.data.features;
+        console.log(mapboxSearchResults.value);
+      } catch {
+        searchError.value = true;
+      }
+
+
+      return;
+    };
+    mapboxSearchResults.value = null;
+  }, 300)
+}
+</script>
